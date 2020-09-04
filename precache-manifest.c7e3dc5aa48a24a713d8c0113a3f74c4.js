b57686f15f3f170012d282c192ba17619b9c6e71
MYK <mohammad.kh.8688@gmail.com>
2020-09-05 00:16:25 +0200

    Updates

---
+++ precache-manifest.c7e3dc5aa48a24a713d8c0113a3f74c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a57c2894680a641b6d6eed05d5852d",
+    "url": "/deploy-portfolio/index.html"
+  },
+  {
+    "revision": "dc8b7bbe98a7c0569d70",
+    "url": "/deploy-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "affa744c95a5f09a7dc0",
+    "url": "/deploy-portfolio/static/js/2.40dc0cf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-portfolio/static/js/2.40dc0cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8b7bbe98a7c0569d70",
+    "url": "/deploy-portfolio/static/js/main.ce0d57a7.chunk.js"
+  },
+  {
+    "revision": "0b7cd3ea6033a18d161b",
+    "url": "/deploy-portfolio/static/js/runtime-main.2ff87b4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
